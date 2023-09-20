9ec6b3e85dc1415801a9dc9a30a7107d2247ae75
Dmitry Burlaka <dem34888@gmail.com>
2023-09-20 10:36:45 +0300

    fix linter

--- packages/server/.eslintrc.cjs
+++ packages/server/.eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  env: { es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    'no-duplicate-imports': ["error", { "includeExports": true }],
-    '@typescript-eslint/consistent-type-imports': 'error',
-    '@typescript-eslint/no-unused-vars': "warn",
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
-  },
-}
+    env: { es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs', '**/node_modules/**'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+        'no-duplicate-imports': ['error', { 'includeExports': true }],
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+};
