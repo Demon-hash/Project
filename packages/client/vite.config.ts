0e757702b20cf6fbc123fe07249a97275246d0f2
Dmitry Burlaka <dem34888@gmail.com>
2023-09-20 10:54:26 +0300

    fix linter

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import codegen from 'vite-plugin-graphql-codegen';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import config from './codegen.ts';
+import config from './codegen';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
